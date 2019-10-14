a673cf0ecf9d323fbfe74afc0050685ebefe2a31
Karina Silva <karina@fas.com.br>
2019-10-14 17:24:50 -0300

    Tela de Acompanhamento de Carteira

--- src/app/main/acompanhamento-carteira/acompanhamento-carteira.component.ts
+++ src/app/main/acompanhamento-carteira/acompanhamento-carteira.component.ts
@@ -1,4 +1,4 @@
-import { ListaRelease } from '../../shared/models/carteira.model';
+import { ListaRelease } from './../../shared/models/carteira.model';
 import { CarteiraService } from '../../shared/services/carteira/carteira.service';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
@@ -13,6 +13,12 @@ export class AcompanhamentoCarteiraComponent implements OnInit {
 
     listaRelease: ListaRelease[];
 
+    releaseAtual: string;
+    releaseFiltradaPorRelease: ListaRelease[];
+    releaseFiltrada: ListaRelease[];
+
+    selected = "todas";
+
     constructor(private carteiraService: CarteiraService, public dialog: MatDialog) { }
 
     ngOnInit() {
@@ -25,10 +31,22 @@ export class AcompanhamentoCarteiraComponent implements OnInit {
                 });
     }
 
-    openDialog(nomeRelease: string, nome: string): void{
+    filtroPorRelease($event) {
+        const release: string = $event.value;
+
+        if(release !== 'todas'){
+            this.releaseFiltradaPorRelease = this.listaRelease.filter((nomeRelease) => { 
+                return nomeRelease === release;
+            });
+
+            this.releaseFiltrada = [...this.releaseFiltradaPorRelease];
+        }
+    }
+
+    openDialog(nomeRelease: string, nome: string): void {
         this.dialog.open(AcompanhamentoIndicadoresComponent, {
             width: '40%',
-            data: {nomeRelease: nomeRelease, nome: nome}
+            data: { nomeRelease: nomeRelease, nome: nome }
         });
     }
 
